105f1f4ce714edb8ba88fbabf159951f4c5076e7
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-05-18 14:20:15 +0530

    status(404)

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const adminShop=require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use((req,res,next)=>{
-    res.send('<h1>ERROR: 404</h1>')
-})
+
 
 app.use(adminRoutes)
 
-app.use(adminShop)
+//app.use(adminShop)
 
 
-
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 
 
